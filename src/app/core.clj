32c9cc65654e78f4f9ca283493419c24d4742c40
Vincenzo Chianese <vincenz.chianese@icloud.com>
2020-06-30 15:46:08 -0500

    pedestal test

--- src/app/core.clj
+++ src/app/core.clj
@@ -1,4 +1,6 @@
-(ns app.core (:require [clojure.spec.alpha :as spec]) (:gen-class))
+(ns app.core (:require [clojure.spec.alpha :as spec]
+                       [io.pedestal.http :as http]
+                       [io.pedestal.http.route :as route]) (:gen-class))
 
 (spec/def ::age integer?)
 (spec/def ::name string?)
@@ -10,6 +12,22 @@
 
 (defn theSum [a b c d] (+ a b c d))
 
+(defn respond-hello [request]
+  {:status 200 :body "Hello, world!"})
+
+(def routes
+  (route/expand-routes
+   #{["/greet" :get respond-hello :route-name :greet]}))
+
+(defn create-server []
+  (http/create-server
+   {::http/routes routes
+    ::http/type   :jetty
+    ::http/port   8890}))
+
+(defn start []
+  (http/start (create-server)))
+
 (defn -main
   []
-  (println (spec/valid? ::user {::name "Vincenzo" ::surname "Chianese" ::age 31})))
+  (start))
