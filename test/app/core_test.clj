4029fb6cf29513fdf64445915c7f96d89ad962b2
Vincenzo Chianese <vincenz.chianese@icloud.com>
2020-09-24 09:55:20 -0500

    add some tests

--- test/app/core_test.clj
+++ test/app/core_test.clj
@@ -1,6 +1,11 @@
 (ns app.core-test
-  (:require [clojure.test :as t]))
+  (:require [clojure.test :as t]
+            [app.depth-seq :as ds]))
 
-(t/deftest a-test
-  (t/testing "I pass"
-    (t/is (= 1 1))))
+(t/deftest depth-seq
+  (t/testing "Returns nil when empty collection"
+    (t/is (= nil (ds/depth-seq []))))
+  (t/testing "Returns the value when the collection has one item"
+    (t/is (= (list (list 10)) (ds/depth-seq [{:value 10}]))))
+  (t/testing "Returns the sum when the collection has more than one item"
+    (t/is (= (list (list 10) (list 20)) (ds/depth-seq [{:value 10 :children [{:value 20}]}])))))
