cf072303702e9036f3a591ff8b9ef783d5829baf
Vincenzo Chianese <vincenz.chianese@icloud.com>
2020-09-24 20:34:54 -0500

    upgrade deps

--- project.clj
+++ project.clj
@@ -4,15 +4,16 @@
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.10.1"]
-                 [com.datomic/client-pro "0.9.57"]
-                 [io.pedestal/pedestal.service "0.5.7"]
-                 [io.pedestal/pedestal.route "0.5.7"]
-                 [io.pedestal/pedestal.jetty "0.5.7"]
+                 [com.datomic/client-pro "0.9.63"]
+                 [io.pedestal/pedestal.service "0.5.8"]
+                 [io.pedestal/pedestal.route "0.5.8"]
+                 [io.pedestal/pedestal.jetty "0.5.8"]
                  [org.clojure/data.json "1.0.0"]
                  [org.slf4j/slf4j-simple "1.7.30"]
-                 [environ "1.1.0"]]
+                 [environ "1.2.0"]]
+  :plugins [[lein-ancient "0.6.15"]]
   :repl-options {:init-ns app.core}
-  :min-lein-version "2.9.3"
-  :profiles {:uberjar {:aot :all}}
+  :min-lein-version "2.9.4"
+  :aot [app.core]
   :uberjar-name "app.jar"
   :main app.core)
