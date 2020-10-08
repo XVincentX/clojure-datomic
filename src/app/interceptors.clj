8f062be045709fef1e6365b70e1f1e6f4fba54f8
Vincenzo Chianese <vincenz.chianese@icloud.com>
2020-10-08 18:28:27 -0500

    add interceptor file

---
+++ src/app/interceptors.clj
@@ -0,0 +1,18 @@
+(ns app.interceptors (:require
+                      [clojure.spec.alpha :as s]
+                      [datomic.client.api :as d]
+                      [app.data :as data]))
+
+(defn validate-payload-shape [source spec]
+  {:name ::validate-query-string-shape
+   :enter (fn [context] (let [request (:request context)
+                              param (get request source)]
+                          (if-let [parsed-param (s/conform spec param)]
+                            (assoc-in context [:request :parsed] parsed-param)
+                            (assoc context :response {:status 412 :headers {}}))))})
+
+(defn with-db []
+  {:name ::with-db
+   :enter (fn [context] (let [conn (d/connect data/client {:db-name "db"})
+                              db (d/db conn)]
+                          (assoc-in context [:request :db] db)))})
