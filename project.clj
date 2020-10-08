2802291f73fa880829625fed200a3ecb7d6c3f43
Vincenzo Chianese <vincenz.chianese@icloud.com>
2020-10-08 16:07:45 -0500

    remove stuff

--- project.clj
+++ project.clj
@@ -1,20 +1,10 @@
-(defproject app "1.0.0"
-  :description "Test application written my be to do stupid stuff"
-  :url "https://github.com/XVincentX/clojure-playground"
-  :license {:name "EPL-2.0"
-            :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :dependencies [[org.clojure/clojure "1.10.1"]
-                 [com.datomic/client-pro "0.9.63"]
-                 [io.pedestal/pedestal.service "0.5.8"]
-                 [io.pedestal/pedestal.route "0.5.8"]
-                 [io.pedestal/pedestal.jetty "0.5.8"]
-                 [org.clojure/data.json "1.0.0"]
-                 [org.slf4j/slf4j-simple "1.7.30"]
-                 [environ "1.2.0"]
-                 [nrepl/drawbridge "0.2.1"]]
-  :plugins [[lein-ancient "0.6.15"]]
-  :repl-options {:init-ns app.core}
-  :min-lein-version "2.9.4"
-  :profiles {:uberjar {:aot :all}}
-  :uberjar-name "app.jar"
-  :main app.core)
+{:deps
+ {com.datomic/dev-local {:mvn/version "0.9.203"}
+  org.clojure/clojure {:mvn/version "1.10.1"}
+  io.pedestal/pedestal.service {:mvn/version "0.5.8"}
+  io.pedestal/pedestal.route {:mvn/version "0.5.8"}
+  io.pedestal/pedestal.jetty {:mvn/version "0.5.8"}
+  org.clojure/data.json {:mvn/version "1.0.0"}
+  org.slf4j/slf4j-simple {:mvn/version "1.7.30"}
+  environ {:mvn/version "1.2.0"}
+  nrepl/drawbridge {:mvn/version "0.2.1"}}}
