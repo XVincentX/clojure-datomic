9e21129465be5ff785ad1821beeb406942c30c50
Vincenzo Chianese <vincenz.chianese@icloud.com>
2020-10-08 20:27:08 -0500

    use built in function

--- src/app/interceptors.clj
+++ src/app/interceptors.clj
@@ -2,20 +2,23 @@
                       [clojure.spec.alpha :as s]
                       [datomic.client.api :as d]
                       [clojure.data.json :as json]
+                      [io.pedestal.interceptor.helpers :as interceptor]
                       [app.data :as data]))
 
 (defn validate-payload-shape [source spec]
-  {:name ::validate-query-string-shape
-   :enter (fn [context] (let [param (get-in context [:request source])
-                              parsed-param (s/conform spec param)]
-                          (if (= parsed-param :clojure.spec.alpha/invalid)
-                            (assoc context :response {:status 412 :headers {} :body (json/write-str (s/explain-str spec param))})
-                            (assoc-in context [:request :parsed] parsed-param))))})
+  (interceptor/on-request
+   ::validate-query-string-shape
+   (fn [context] (let [param (get-in context [:request source])
+                       parsed-param (s/conform spec param)]
+                   (if (= parsed-param :clojure.spec.alpha/invalid)
+                     (assoc context :response {:status 412 :headers {} :body (json/write-str (s/explain-str spec param))})
+                     (assoc-in context [:request :parsed] parsed-param))))))
 
-(defn with-db []
-  {:name ::with-db
-   :enter (fn [context] (let [conn (d/connect data/client {:db-name "db"})
-                              db (d/db conn)]
-                          (-> context
-                              (assoc-in [:request :db] db)
-                              (assoc-in [:request :conn] conn))))})
+(def with-db (interceptor/on-request
+              ::with-db
+              (fn [context]
+                (let [conn (d/connect data/client {:db-name "db"})
+                      db (d/db conn)]
+                  (-> context
+                      (assoc-in [:request :db] db)
+                      (assoc-in [:request :conn] conn))))))
