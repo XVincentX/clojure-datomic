52cfaa1e9bf60c5a3546474ef3249518be64c0f5
Vincenzo Chianese <vincenz.chianese@icloud.com>
2020-10-08 20:21:03 -0500

    short circuit

--- src/app/interceptors.clj
+++ src/app/interceptors.clj
@@ -1,15 +1,16 @@
 (ns app.interceptors (:require
                       [clojure.spec.alpha :as s]
                       [datomic.client.api :as d]
+                      [clojure.data.json :as json]
                       [app.data :as data]))
 
 (defn validate-payload-shape [source spec]
   {:name ::validate-query-string-shape
-   :enter (fn [context] (let [request (:request context)
-                              param (get request source)]
-                          (if-let [parsed-param (s/conform spec param)]
-                            (assoc-in context [:request :parsed] parsed-param)
-                            (assoc context :response {:status 412 :headers {}}))))})
+   :enter (fn [context] (let [param (get-in context [:request source])
+                              parsed-param (s/conform spec param)]
+                          (if (= parsed-param :clojure.spec.alpha/invalid)
+                            (assoc context :response {:status 412 :headers {} :body (json/write-str (s/explain-str spec param))})
+                            (assoc-in context [:request :parsed] parsed-param))))})
 
 (defn with-db []
   {:name ::with-db
