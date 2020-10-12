8babb1e0da9c310d8c64db15621b82ebea9bdf89
Vincenzo Chianese <vincenz.chianese@icloud.com>
2020-10-12 09:21:32 -0500

    use uuids

--- src/app/core.clj
+++ src/app/core.clj
@@ -14,18 +14,26 @@
     ::http/port   (Integer. (or (env :port) 5000))}))
 
 (defn get-user-by-id [db id]
-  (d/pull db '[:person/name :person/surname] id))
+  (d/q '[:find ?name ?surname
+         :in $ id
+         :where
+         [_ :person/id ?id]
+         [_ :person/name ?name]
+         [_ :person/surname ?surname]] db id))
 
 (defn get-users-id [db]
-  (d/q '[:find ?e :in $ :where [?e :person/name _]] db))
+  (d/q '[:find ?id :in $ :where [_ :person/id ?id]] db))
 
-(defn add-user [conn data] (d/transact conn {:tx-data [data]}))
+(defn add-user [conn data]
+  (let [id (java.util.UUID/randomUUID)]
+    (d/transact conn {:tx-data [(assoc data :person/id id)]})
+    id))
 
 (def routes
   (route/expand-routes
    #{["/people/:id" :get
       [(interceptors/with-db)
-       #(let [result (get-user-by-id (:db %) (Long. (get-in % [:path-params :id])))]
+       #(let [result (get-user-by-id (:db %) (java.util.UUID/fromString (get-in % [:path-params :id])))]
           {:status 200 :body result})]
       :route-name :get-person]
 
@@ -39,7 +47,8 @@
       [(body-params)
        (interceptors/validate-payload-shape :json-params :app.data/person)
        (interceptors/with-db)
-       #(let [_ (add-user (:conn %) (:parsed %))] {:status 201})]
+       (fn [req] (let [id (add-user (:conn req) (:parsed req))]
+                   {:status 201 :body (str id)}))]
       :route-name :add-people]}))
 
 (defonce server (atom nil))
