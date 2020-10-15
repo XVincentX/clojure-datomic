9ed849c90964dd27f1d7f643da94af0aa900bc54
Vincenzo Chianese <vincenz.chianese@icloud.com>
2020-10-14 20:24:29 -0500

    return old db when requested

--- src/app/interceptors.clj
+++ src/app/interceptors.clj
@@ -5,19 +5,21 @@
                       [app.data :as data]))
 
 (defn validate-payload-shape [source spec]
-  (interceptor/before ::validate-query-string-shape
-                      #(let [param (get-in % [:request source])
-                             parsed-param (s/conform spec param)]
-                         (if (= parsed-param :clojure.spec.alpha/invalid)
-                           (assoc % :response {:status 412
-                                               :headers {"Content-Type" "text/plain"}
-                                               :body (s/explain-str spec param)})
-                           (assoc-in % [:request :parsed] parsed-param)))))
+  (interceptor/before
+   ::validate-query-string-shape
+   #(let [param (get-in % [:request source])
+          parsed-param (s/conform spec param)]
+      (if (= parsed-param :clojure.spec.alpha/invalid)
+        (assoc % :response {:status 412
+                            :headers {"Content-Type" "text/plain"}
+                            :body (s/explain-str spec param)})
+        (assoc-in % [:request :parsed] parsed-param)))))
 
 (def with-db
-  (interceptor/on-request ::with-db
-                          #(let [conn (d/connect data/client {:db-name data/db-name})
-                                 db (d/db conn)]
-                             (-> %
-                                 (assoc :db db)
-                                 (assoc :conn conn)))))
+  (interceptor/on-request
+   ::with-db
+   #(let [conn (d/connect data/client {:db-name data/db-name})
+          db (d/db conn)]
+      (-> %
+          (assoc :db (if-let [t (get-in % [:query-params :t])] (d/as-of db t) db))
+          (assoc :conn conn)))))
