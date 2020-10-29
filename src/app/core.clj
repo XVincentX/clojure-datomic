11fb7398f5f068949eb90da2ef9e63462691b09c
Vincenzo Chianese <vincenz.chianese@icloud.com>
2020-10-28 23:00:42 -0500

    add tx time to the queries

--- src/app/core.clj
+++ src/app/core.clj
@@ -15,18 +15,27 @@
       (http/default-interceptors)
       (update ::http/interceptors conj interceptors/with-db)
       (update ::http/interceptors conj interceptors/early-304)
+      (update ::http/interceptors conj interceptors/tx-304)
       (update ::http/interceptors conj interceptors/caching-headers)
       (update ::http/interceptors conj http/json-body)
       http/create-server))
 
 (defn get-user-by-id [db id]
-  (d/q '[:find (pull ?e [:person/id :person/name :person/surname])
+  (d/q '[:find (pull ?e [:person/id :person/name :person/surname]) ?t
          :in $ ?id
-         :where [?e :person/id ?id]] db id))
+         :where
+         [?e :person/id ?id ?txId]
+         [?e :person/name _ ?txName]
+         [?e :person/surname _ ?txSurname]
+         [(max ?txId ?txName ?txSurname) ?t]] db id))
 
 (defn get-all-users [db]
-  (d/q '[:find (pull ?e [:person/id :person/name :person/surname])
-         :where [?e :person/id]] db))
+  (d/q '[:find (pull ?e [:person/id :person/name :person/surname]) ?t
+         :where
+         [?e :person/id _ ?txId]
+         [?e :person/name _ ?txName]
+         [?e :person/surname _ ?txSurname]
+         [(max ?txId ?txName ?txSurname) ?t]] db))
 
 (defn add-user [conn data]
   (let [id (java.util.UUID/randomUUID)]
